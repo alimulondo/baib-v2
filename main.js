a3a50d8ca820088885d2b4a7a78bc25d7b59e2dd
alimulondo <myalimul@gmail.com>
2019-03-20 08:57:12 -0400

    edit index

---
+++ main.js
@@ -0,0 +1,61 @@
+
+function eldery(){
+	$('#eldery').on('click', function () {
+    $.fn.custombox( this, {
+        overlay: true,
+        effect: 'newspaper'
+    });
+    return false;
+});
+}
+
+
+function houseKeeping(){
+	$('#housek').on('click', function () {
+    $.fn.custombox( this, {
+        overlay: true,
+        effect: 'newspaper'
+    });
+    return false;
+});
+}
+
+function babySeating(){
+	$('#babys').on('click', function () {
+    $.fn.custombox( this, {
+        overlay: true,
+        effect: 'newspaper'
+    });
+    return false;
+});
+}
+
+function africanCulinary(){
+	$('#africanc').on('click', function () {
+    $.fn.custombox( this, {
+        overlay: true,
+        effect: 'newspaper'
+    });
+    return false;
+});
+}
+
+function fullPackage(){
+	$('#fullp').on('click', function () {
+    $.fn.custombox( this, {
+        overlay: true,
+        effect: 'newspaper'
+    });
+    return false;
+});
+}
+
+function internationalCuisine(){
+	$('#intercuisine').on('click', function () {
+    $.fn.custombox( this, {
+        overlay: true,
+        effect: 'newspaper'
+    });
+    return false;
+});
+}
